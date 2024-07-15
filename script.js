17797585b346bae12e4202d1e2614b4b43960118
Ahladaaadrija <152667720+Ahladaaadrija@users.noreply.github.com>
2024-07-15 10:57:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,81 @@
 const cells = document.querySelectorAll(".cell");
-const statustext = document.querySelector("#statustext");
-const restartbtn = document.querySelector("#restartbtn");
+const statusText = document.querySelector("#statustext");
+const restartBtn = document.querySelector("#restartbtn");
+
 const winconditions = [
-    [0, 1, 2],
-    [3, 4, 5], 
-    [6, 7, 8], 
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8], 
-    [0, 4, 8], 
-    [2, 4, 6]
+     [0, 1, 2],
+     [3, 4, 5],
+     [6, 7, 8],
+     [0, 3, 6],
+     [1, 4, 7],
+     [2, 5, 8],
+     [0, 4, 8],
+     [2, 4, 6] 
 ];
 let options = ["", "", "", "", "", "", "", "", ""];
-let currentplayer = "X";
+let currentPlayer = "X";
 let running = false;
 
-initializegame();
+initializeGame();
 
-function initializegame(){
-    cells.forEach(cell => cell.addEventListener("click", cellclicked));
-    restartbtn.addEventListener("click", restartgame);
-    statustext.textContent = `${currentplayer}'s turn`;
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
 }
+function cellClicked(){
+      const cellindex = this.getAttribute("cellindex");
 
-function cellclicked(){
-    const cellindex = this.getAttribute("cellindex");
-
-    if(options[cellindex] != "" || !running){
+      if(options[cellindex] != "" || !running){
         return;
-    }
+      }
 
-    updatecell(this, cellindex);
-    checkwinner();
-
+      updateCell(this, cellindex);
+      checkwinner();
 }
-
-function updatecell(cell, index){
-    options[index] = currentplayer;
-    cell.textContent = currentplayer;
-
+function updateCell(cell, index){
+        options[index] = currentPlayer;
+        cell.textContent = currentPlayer;
 }
-
-function changeplayer(){
-    currentplayer = (currentplayer == "X" ) ? "O" : "X";
-    statustext.textContent = `${currentplayer}'s turn`;
-
+function changePlayer(){
+     currentPlayer = (currentPlayer == "X") ? "O" : "X";
+     statusText.textContent = `${currentPlayer}'s turn`;
 }
-
 function checkwinner(){
-    let roundwon = false;
+      let roundWon = false;
 
-    for(let i = 0; i < winconditions.length;  i++){
-        const condition = winconditions[i];
-        const cellA = options[condition[0]];
-        const cellB = options[condition[1]];
-        const cellC = options[condition[2]];
+      for(let i = 0; i < winconditions.length; i++){
+        const conditions = winconditions[i];
+        const cellA = options[conditions[0]];
+        const cellB = options[conditions[1]];
+        const cellC = options[conditions[2]];
 
         if(cellA == "" || cellB == "" || cellC == ""){
             continue;
         }
-        if(cellA == cellB  && cellB == cellC){
-            roundwon = true;
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
             break;
         }
-    }
-    if(roundwon){
-        statustext.textContent = `${currentplayer} wins!`;
+      }
+
+      if(roundWon){
+        statusText.textContent = `${currentPlayer} wins`;
         running = false;
-    }
-    else if(!option.includes("")){
-        statustext.textContent = `Draw!`;
-        running = false;
-    }
-    else{
-        changeplayer();
-    }
-function restartgame(){
-    currentplayer = "X";
-    option = ["", "", "", "", "", "", "", "", ""];
-    statustext.textContent = `${currentplayers}'s turn`;
+      }
+      else if(!options.includes("")){
+             statusText.textContent = `DRAW!`;
+             running = false;
+      }
+      else{
+        changePlayer();
+      }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s Turn`;
     cells.forEach(cell => cell.textContent = "");
     running = true;
-
-
-}
 }
